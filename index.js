34bf202c6d0219384cdfef2bdb98bc939d1e404b
alid994 <aljo_dozer@hotmail.com>
2023-01-04 19:01:19 +0100

    prvi korak

---
+++ index.js
@@ -0,0 +1,25 @@
+/*V = r2 * π * H
+
+r – poluprečnik osnove valjka
+H – visina
+π – konstanta Pi, približno 3,14*/
+
+function zapremina() {
+    var precnik = document.getElementById("precnik").value;
+    var visina = document.getElementById("visina").value;
+    const PI = 3.14;
+
+    var rez = precnik * precnik * PI * visina;
+    var c = document.getElementById("rez").innerHTML = `Zapremina valjka je: ${rez} cm2`;
+};
+
+var input = document.getElementById("visina");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("dugme").click();
+}
+});
+function prikaz() {
+  var pr = document.getElementById("wrapper").style.visibility = "hidden";
+}
